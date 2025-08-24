eed8e6a4c0d1809300f6c74b180cc99ea9393314
muhammadibraheem <muibraheem662@gmail.com>
2025-08-24 22:58:13 +0800

    first

---
+++ project/java.js
@@ -0,0 +1,17 @@
+// schedule table 
+function scheduleTabs() {
+    const tabs = document.querySelectorAll(".js-schedule-tab");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+            if (tab.classList.contains("active")) {
+                return;
+            }
+            document.querySelector(".js-schedule-tab.active").classList.remove("active");
+            tab.classList.add("active");
+            const target = tab.getAttribute("data-target");
+            document.querySelector(".js-schedule-table.active").classList.remove("active");
+            document.querySelector(target).classList.add("active");
+        })
+    });
+}
